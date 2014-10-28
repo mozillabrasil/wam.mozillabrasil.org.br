83b2ce09531905704e7e8e069a2ed339b6fb97fb
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-10-27 22:53:54 -0200

    l10n em produtos -> webmaker

--- application/views/products/webmaker.php
+++ application/views/products/webmaker.php
@@ -9,13 +9,9 @@
     <div class="row">
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="" class="col-lg-6 line-height-52px" style="line-height: 52px;">
-                    Ensinamos a web
-                </h6>
+                <h6 data-l10n-id="PRODUCTS_WEBMAKER_TITLE" class="col-lg-6 line-height-52px" style="line-height: 52px;"></h6>
                 <h6 class="col-lg-6 text-right">
-                    <a data-l10n-id="" href="https://webmaker.org/" target="_new" class="pm-slide-btn animated no-margin">
-                        Quero saber mais
-                    </a>
+                    <a data-l10n-id="PRODUCTS_WEBMAKER_DESCRIPTION" href="https://webmaker.org/" target="_new" class="pm-slide-btn animated no-margin"></a>
                 </h6>
             </div>
         </div>
@@ -23,8 +19,8 @@
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Crie e remixe a web com as ferramentas do Webmaker</h6>
-                <p data-l10n-id="" class="text">As ferramentas do WebMaker torna mais fácil de criar coisas na web. Faça as suas próprias páginas web, vídeos interativos, remixes, aplicativos móveis e muito mais, aprender a mecânica da web, de código e outras habilidades valiosas.</p>
+                <h6 data-l10n-id="PRODUCTS_WEBMAKER_REMIX_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_WEBMAKER_REMIX_DESCRIPTION" class="text"></p>
             </div>
         </div>
     </div>
@@ -32,41 +28,33 @@
         <div class="col-lg-3 col-md-3 col-sm-12 text-center pm-sidebar">
             <img alt="" src="<?php echo base_url(); ?>assets/images/products/webmaker/icon-tools-xray-2x.png" height="100">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Óculos de Raio-X</h6>
-                <p data-l10n-id="" class="text text-justify">Óculos de Raio-X permite que você veja as partes constituintes de um site. Ative os óculos para inspecionar o código por trás de qualquer página web, então remixe elementos com um simples clique, modificando-a com seus próprios textos, imagens e mais.</p>
-                <a data-l10n-id="" href="https://goggles.webmaker.org/pt-BR" target="_new" class="pm-slide-btn animated no-margin">
-                    Experimente os Goggles agora
-                </a>
+                <h6>X-Ray Goggles</h6>
+                <p data-l10n-id="PRODUCTS_WEBMAKER_X_RAY_DESCRIPTION" class="text text-justify"></p>
+                <a data-l10n-id="PRODUCTS_WEBMAKER_X_RAY_BUTTON" href="https://goggles.webmaker.org/pt-BR" target="_new" class="pm-slide-btn animated no-margin"></a>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-12 text-center pm-sidebar">
             <img alt="" src="<?php echo base_url(); ?>assets/images/products/webmaker/icon-tools-thimble-2x.png" height="100">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Thimble</h6>
-                <p data-l10n-id="" class="text text-justify">O Thimble torna ridiculamente simples o processo de criação e compartilhamento de suas páginas web. Escreva e edite HTML e CSS diretamente do navegador, então visualize instantaneamente suas mudanças. Hospede e compartilhe seus projetos com um simples clique. Perfeito para iniciantes e especialistas.</p>
-                <a data-l10n-id="" href="https://thimble.webmaker.org/pt-BR" target="_new" class="pm-slide-btn animated no-margin">
-                    Começar do zero
-                </a>
+                <h6>Thimble</h6>
+                <p data-l10n-id="PRODUCTS_WEBMAKER_THIMBLE_DESCRIPTION" class="text text-justify"></p>
+                <a data-l10n-id="PRODUCTS_WEBMAKER_THIMBLE_BUTTON" href="https://thimble.webmaker.org/pt-BR" target="_new" class="pm-slide-btn animated no-margin"></a>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-12 text-center pm-sidebar">
             <img alt="" src="<?php echo base_url(); ?>assets/images/products/webmaker/icon-tools-popcorn-2x.png" height="100">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Popcorn Maker</h6>
-                <p data-l10n-id="" class="text text-justify">O Popcorn Maker ajuda você a facilmente remixar vídeos, sons e imagens da web, criando montagens que você pode integrar em outros sites. Arraste e solte conteúdos da web, adicione seus comentários e links — tudo a partir do seu navegador. Os vídeos criados com o Popcorn Maker são dinâmicos, cheios de links e únicos a cada visualização.</p>
-                <a data-l10n-id="" href="https://popcorn.webmaker.org/pt-BR/editor/" target="_new" class="pm-slide-btn animated no-margin">
-                    Começar do zero
-                </a>
+                <h6>Popcorn Maker</h6>
+                <p data-l10n-id="PRODUCTS_WEBMAKER_POPCORN_DESCRIPTION" class="text text-justify"></p>
+                <a data-l10n-id="PRODUCTS_WEBMAKER_POPCORN_BUTTON" href="https://popcorn.webmaker.org/pt-BR/editor/" target="_new" class="pm-slide-btn animated no-margin"></a>
             </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-12 text-center pm-sidebar">
             <img alt="" src="<?php echo base_url(); ?>assets/images/products/webmaker/logo_brickphone_phone_only.png" height="100">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Appmaker</h6>
-                <p data-l10n-id="" class="text text-justify">Appmaker é uma ferramenta gratuita para criação de aplicativos móveis, mesmo se você não souber programar! Combine diferentes partes para criar e compartilhar aplicativos móveis customizados em seu navegador. </p>
-                <a data-l10n-id="" href="https://apps.webmaker.org/pt-BR/designer" target="_new" class="pm-slide-btn animated no-margin">
-                    Começar do zero
-                </a>
+                <h6>Appmaker</h6>
+                <p data-l10n-id="PRODUCTS_WEBMAKER_APPMAKER_DESCRIPTION" class="text text-justify"></p>
+                <a data-l10n-id="PRODUCTS_WEBMAKER_APPMAKER_BUTTON" href="https://apps.webmaker.org/pt-BR/designer" target="_new" class="pm-slide-btn animated no-margin"></a>
             </div>
         </div>
     </div>
