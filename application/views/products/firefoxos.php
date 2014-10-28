cf118871753cce689d7f8d8f97bf91e4a2f38c87
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-10-27 22:20:43 -0200

    l10n em produtos -> firefox os

--- application/views/products/firefoxos.php
+++ application/views/products/firefoxos.php
@@ -9,13 +9,9 @@
     <div class="row">
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="" class="col-lg-6 line-height-52px" style="line-height: 52px;">
-                    Feito para os momentos da sua vida
-                </h6>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_TITLE" class="col-lg-6 line-height-52px" style="line-height: 52px;"></h6>
                 <h6 class="col-lg-6 text-right">
-                    <a data-l10n-id="" href="http://www.vivo.com.br/firefox/" target="_new" class="pm-slide-btn animated no-margin">
-                        Obtenha um smartphone Firefox OS hoje
-                    </a>
+                    <a data-l10n-id="PRODUCTS_FIREFOXOS_DESCRIPTION" href="http://www.vivo.com.br/firefox/" target="_new" class="pm-slide-btn animated no-margin"></a>
                 </h6>
             </div>
         </div>
@@ -26,16 +22,16 @@
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Da nossa comunidade para a sua</h6>
-                <p data-l10n-id="" class="text">A maioria dos smartphones é feita para um pequeno número de pessoas, todas no mesmo lugar, provavelmente bem longe de onde você vive. Telefones Firefox OS são diferentes. São feitos por uma grande comunidade global e são os únicos smartphones que entendem e oferecem o que você quer e precisa.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_COMMUNITY_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_COMMUNITY_DESCRIPTION" class="text"></p>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">O melhor da Web</h6>
-                <p data-l10n-id="" class="text">O Firefox mudou a maneira de navegar para sempre ao colocar os usuários em primeiro lugar e oferecendo um novo nível de escolha e inovação on-line. Agora estamos fazendo isso novamente com o Firefox OS, ao trazer a mesma experiência superior de navegação ao seu smartphone. Assim você pode tomar o controle da sua vida on-line e obter o melhor que a Web oferece não importa onde você vá.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_BEST_OF_WEB_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_BEST_OF_WEB_DESCRIPTION" class="text"></p>
             </div>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12">
@@ -45,27 +41,27 @@
     <div class="row">
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Faça cada momento valer a pena</h6>
-                <p data-l10n-id="" class="text">Suas necessidades mudam de uma hora para outra. É por isso que o Firefox OS faz o mesmo. Ele oferece opções para viver cada momento ao máximo e interagir com um mundo melhor.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_GREAT_MOMENTS_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_GREAT_MOMENTS_DESCRIPTION" class="text"></p>
             </div>
         </div>
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Ele se adapta para que você também possa</h6>
-                <p data-l10n-id="" class="text">O Firefox OS antecipa suas necessidades, se adapta a cada situação e oferece instantaneamente a informação que você deseja</p>
-                <p data-l10n-id="" class="text">Isso inclui aplicativos de serviços populares, mas também conteúdo local com mais relevância para você e sua vida.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_ADAPT_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_ADAPT_DESCRIPTION_1" class="text"></p>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_ADAPT_DESCRIPTION_2" class="text"></p>
             </div>
         </div>
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Tenha tudo</h6>
-                <p data-l10n-id="" class="text">O Firefox OS vem com os recursos que fazem você desejar um smartphone. Ele se ajusta à sua vida perfeitamente para que você se ajuste ao mundo ao seu redor.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_GET_ALL_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_GET_ALL_DESCRIPTION" class="text"></p>
             </div>
         </div>
         <div class="col-lg-12 pm-sidebar text-center">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Transformar o futuro</h6>
-                <p data-l10n-id="" class="text">Estamos aqui para fazer a diferença, não lucros. Ao escolher o Firefox OS, você ajudará a construir um futuro brilhante para a Web e usuários de todo o mundo.</p>
+                <h6 data-l10n-id="PRODUCTS_FIREFOXOS_BE_THE_FUTURE_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_FIREFOXOS_BE_THE_FUTURE_DESCRIPTION" class="text"></p>
             </div>
         </div>
     </div>
