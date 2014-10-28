101e45ba8876aa4bab7817582827835baf5c539a
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-10-27 22:03:33 -0200

    l10n em newsletter

--- application/views/subscribe/add.php
+++ application/views/subscribe/add.php
@@ -1,17 +1,17 @@
 <div class="pm-sub-header-container pm-parallax-panel subscribe" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="0">
     <div class="pm-sub-header-title-container">
-        <p class="pm-sub-header-title"><span data-l10n-id="">Participe</span></p>
-        <p data-l10n-id="" class="pm-sub-header-message">E construa uma web melhor!</p>
+        <p class="pm-sub-header-title"><span data-l10n-id="SUBSCRIBE_TITLE"></span></p>
+        <p data-l10n-id="SUBSCRIBE_DESCRIPTION" class="pm-sub-header-message"></p>
     </div>
 </div>
 <div class="container pm-containerPadding-top-60">
     <div class="row">
         <div class="col-lg-12">
-            <h2 data-l10n-id="">Obrigado</h2>
-            <p data-l10n-id="" class="text">Obrigado por se cadastrar em nossa newsletter.</p>
-            <p data-l10n-id="" class="text">Agora você faz parte da lista exclusiva que recebe as atualizações da WAM !</p>
-            <p data-l10n-id="" class="text text-center">
-                <a data-l10n-id="" href="<?php echo base_url();?>#home">Voltar à página inicial.</a>
+            <h2 data-l10n-id="SUBSCRIBE_THANKYOU_TITLE"></h2>
+            <p data-l10n-id="SUBSCRIBE_THANKYOU_DESCRIPTION_1" class="text"></p>
+            <p data-l10n-id="SUBSCRIBE_THANKYOU_DESCRIPTION_2" class="text"></p>
+            <p class="text text-center">
+                <a data-l10n-id="SUBSCRIBE_THANKYOU_BUTTON" href="<?php echo base_url();?>#home"></a>
             </p>
         </div>
     </div>
