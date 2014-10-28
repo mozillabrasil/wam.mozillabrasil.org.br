24c9af3314a53b2436aa7d173a14676e8f57d2d3
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-10-27 22:46:04 -0200

    l10n em produtos -> marketplace

--- application/views/products/marketplace.php
+++ application/views/products/marketplace.php
@@ -9,13 +9,9 @@
     <div class="row">
         <div class="col-lg-12 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="" class="col-lg-6 line-height-52px" style="line-height: 52px;">
-                    Plataforma de aplicativos da mozilla
-                </h6>
+                <h6 data-l10n-id="PRODUCTS_MARKETPLACE_TITLE" class="col-lg-6 line-height-52px" style="line-height: 52px;"></h6>
                 <h6 class="col-lg-6 text-right">
-                    <a data-l10n-id="" href="https://marketplace.firefox.com/" target="_new" class="pm-slide-btn animated no-margin">
-                        Quero acessar a Marketplace
-                    </a>
+                    <a data-l10n-id="PRODUCTS_MARKETPLACE_DESCRIPTION" href="https://marketplace.firefox.com/" target="_new" class="pm-slide-btn animated no-margin"></a>
                 </h6>
             </div>
         </div>
@@ -23,20 +19,20 @@
     <div class="row">
         <div class="col-lg-4 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Projete</h6>
-                <p data-l10n-id="" class="text">Saiba como criar aplicativos que oferecam uma experiência ao usuário otimizada para o Firefox OS.</p>
+                <h6 data-l10n-id="PRODUCTS_MARKETPLACE_PROJECTS_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_MARKETPLACE_PROJECTS_DESCRIPTION" class="text"></p>
             </div>
         </div>
         <div class="col-lg-4 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Construa</h6>
-                <p data-l10n-id="" class="text">Todas as ferramentas, documentos e referências que você precisa para desenvolvimento e testar o seu aplicativo</p>
+                <h6 data-l10n-id="PRODUCTS_MARKETPLACE_BUILD_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_MARKETPLACE_BUILD_DESCRIPTION" class="text"></p>
             </div>
         </div>
         <div class="col-lg-4 pm-sidebar">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Publique</h6>
-                <p data-l10n-id="" class="text"> Saiba como distribuir seus aplicativos em um mercado aberto que coloca os usuários e desenvolvedores em primeiro lugar </p>
+                <h6 data-l10n-id="PRODUCTS_MARKETPLACE_PUBLISH_TITLE"></h6>
+                <p data-l10n-id="PRODUCTS_MARKETPLACE_PUBLISH_DESCRIPTION" class="text"></p>
             </div>
         </div>
     </div>
@@ -45,33 +41,31 @@
             <img src="<?php echo base_url(); ?>assets/images/products/marketplace/rocket.png" alt="image3">
         </div>
         <div class="col-lg-12 text-center">
-            <a data-l10n-id="" href="https://marketplace.firefox.com/" target="_new" class="pm-slide-btn animated no-margin">
-                Tenho um aplicativo e quero enviar para Marketplace
-            </a>
+            <a data-l10n-id="PRODUCTS_MARKETPLACE_SUBMIT_A_APP" href="https://marketplace.firefox.com/" target="_new" class="pm-slide-btn animated no-margin"></a>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-3 pm-sidebar text-center">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Firefox OS</h6>
+                <h6>Firefox OS</h6>
                 <img src="<?php echo base_url(); ?>assets/images/products/marketplace/device-ffos.png" alt="image3">
             </div>
         </div>
         <div class="col-lg-3 pm-sidebar text-center">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Telefones Android</h6>
+                <h6>Android Phones</h6>
                 <img src="<?php echo base_url(); ?>assets/images/products/marketplace/device-android.png" alt="image3">
             </div>
         </div>
         <div class="col-lg-3 pm-sidebar text-center">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Tablets Android</h6>
+                <h6>Android Tablets</h6>
                 <img src="<?php echo base_url(); ?>assets/images/products/marketplace/device-tablet.png" alt="image3">
             </div>
         </div>
         <div class="col-lg-3 pm-sidebar text-center">
             <div class="pm-widget">
-                <h6 data-l10n-id="">Computador</h6>
+                <h6>PC</h6>
                 <img src="<?php echo base_url(); ?>assets/images/products/marketplace/device-browser.png" alt="image3">
             </div>
         </div>
